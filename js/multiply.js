9064e105cbfdea5d211d0902627409dbecd9f6cb
Ephraim Acquah <ephraimacquah14@gmail.com>
2023-11-14 23:15:31 -0500

    comments

--- js/multiply.js
+++ js/multiply.js
@@ -1,15 +1,19 @@
+/*
+Name: Ephraim Acquah
+Email: Ephraim_Acquah@student.uml.edu
+*/
 function fillTable() {
-    if(validateForm()) {
-        clearTable();
-        var times_table = document.getElementById("times_table");
-        var x_min = Number(document.getElementById("x_begin").value);
-        var x_max = Number(document.getElementById("x_end").value);
-        var y_min = Number(document.getElementById("y_begin").value);
-        var y_max = Number(document.getElementById("y_end").value);
+    if(validateForm()) { //if the form is valid then continue to fill the table
+        clearTable(); //clear any existing table that has been generated
+        var times_table = document.getElementById("times_table"); //get table var
+        var x_min = Number(document.getElementById("x_begin").value); //get min col
+        var x_max = Number(document.getElementById("x_end").value); //get max col
+        var y_min = Number(document.getElementById("y_begin").value); //get min row
+        var y_max = Number(document.getElementById("y_end").value); //get max row
         var curr_row;
         var curr_cell;
-        times_table.insertRow(0).insertCell(0).innerHTML = "X";
-        for(var i = 1; i <= y_max - y_min + 1; i++) {
+        times_table.insertRow(0).insertCell(0).innerHTML = "X"; //fill in top left cell
+        for(var i = 1; i <= y_max - y_min + 1; i++) { // fills in row label column with min to max rows and sets styling
             var th = document.createElement("th");
             th.innerHTML = i + y_min - 1;
             th.style.backgroundColor = "bisque";
@@ -18,7 +22,7 @@ function fillTable() {
             curr_row.appendChild(th);
         }
 
-        for(i = 1; i <= x_max - x_min + 1; i++) {
+        for(i = 1; i <= x_max - x_min + 1; i++) { // fills in column label row with min to max columns and sets styling
             var th = document.createElement("th");
             th.innerHTML = i - 1 + x_min;
             th.style.backgroundColor = "bisque";
@@ -26,7 +30,7 @@ function fillTable() {
             times_table.rows[0].appendChild(th); 
         }
 
-        for(i = 1; i <= y_max - y_min + 1; i++) {
+        for(i = 1; i <= y_max - y_min + 1; i++) { //goes through each row and sets cell value to product of row and column
             curr_row = times_table.rows[i];
             for(var j = 1; j <= x_max - x_min + 1; j++) {
                 curr_cell = curr_row.insertCell(j);
@@ -35,7 +39,7 @@ function fillTable() {
                 curr_cell.innerHTML = (i + y_min - 1) * (j + x_min - 1);
             }
         }
-        document.getElementById("x_begin").value = '';
+        document.getElementById("x_begin").value = ''; //resets form values
         document.getElementById("x_end").value = '';
         document.getElementById("y_begin").value = '';
         document.getElementById("y_end").value = '';
@@ -43,14 +47,14 @@ function fillTable() {
 }
 
 function validateForm(){
-  let x = document.forms["form"]["x_begin"].value;
+  let x = document.forms["form"]["x_begin"].value; //gets x begin value, minimum column value and if greater than maximum column value, leaves an error message
   if (x > document.forms["form"]["x_end"].value) {
     document.getElementById("error").style.color = "brown";
     document.getElementById("error").style.textAlign = "center";
     document.getElementById("error").innerHTML = "Column Maximum Value must be greater than Column Minimum Value";
     return false;
   }
-  x = document.forms["form"]["y_begin"].value;
+  x = document.forms["form"]["y_begin"].value; //gets x begin value, minimum column value and if greater than maximum column value, leaves an error message
   if(x > document.forms["form"]["y_end"].value){
     document.getElementById("error").style.color = "brown";
     document.getElementById("error").style.textAlign = "center";
@@ -63,7 +67,7 @@ function validateForm(){
 function clearTable() {
     table =  document.getElementById("times_table")
     len = table.rows.length;
-    for(var i = 0; i < len; i++) {
+    for(var i = 0; i < len; i++) { //deletes each row of the table
         table.deleteRow(0);
      }
 }
